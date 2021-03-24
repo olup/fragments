677a0b54d2f38b4f91ad8290fb4081e5975f01a7
Olup <loup.topalian@gmail.com>
2021-03-24 11:32:40 +0100

    update

--- packages/api/src/auth.ts
+++ packages/api/src/auth.ts
@@ -4,6 +4,7 @@ import { APIGatewayEvent, Context } from "aws-lambda";
 import fetch from "node-fetch";
 export async function handler(event: APIGatewayEvent, context: Context) {
   const { code } = event.queryStringParameters as any;
+  if (!code) throw new Error("The code parameter is required");
   const response = await fetch(
     `https://github.com/login/oauth/access_token?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&code=${code}&redirect_uri=http://localhost:3000&scope=repo user`
   );
